02a44e96dde3bfc4964097f103db784c45ced92a
ayemyathida <ayemya.thida87@gmail.com>
2023-08-04 13:08:00 +0800

    Added fakeAPI and reducer

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,39 +1,73 @@
+import { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
-const BookForm = (props) => {
+const BookForm = ({availableTimes, dispatchOnDateChange, submitData}) => {
+
+  const availableTimesOptions = availableTimes.map((item, index) => {
+      return <option key={index} value={item}>{item}</option>;
+  });
 
   const occasionType = ["Birthday", "Anniversary"];
-
   const occasionTypeOptions = occasionType.map((item) => {
     return <option key={item}>{`${item}`}</option>;
   });
 
-  const availableTimesOptions = props.availableTimes.map((item) => {
-    return <option key={item} value={item}>{`${item}:00`}</option>;
-  });
+  useEffect(() => {
+
+    console.log(availableTimes);
+
+  }, []);
+
+  // const handleChange = (e) => {
+  //   const name = e.target.name;
+  //   const value = e.target.value;
+  //   setForm((values) => ({ ...values, [name]: value }));
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   if (!date) {
+  //     setIsDisable(true);
+  //   }
+
+  //   console.log(date, times, guests, occasion);
+  //   setTimeout(() => {
+  //     dispatch({ type: "initializeTimes", time: initializeTimes.time });
+  //     console.log("Done Dispatch time");
+  //   }, 1000);
+
+  //   setTimeout(() => {
+  //     console.log("After setTimeout, time: ", times);
+  //   }, 2000);
+  // };
+
+  const handleDateChange = (e) => {
+    console.log('handleDateChange Reach');
+    dispatchOnDateChange({ type: "updateTimes", date: e.target.value });
+    formik.handleChange(e);
+  };
 
   const bookingSchema = Yup.object().shape({
     date: Yup.string().required("Required"),
     time: Yup.string().required("Required"),
-    guests: Yup.number()
-    .min(1, 'Mininum should be 1')
-    .max(10, 'Maximum should be 10')
-    .required("Required"),
+    guests: Yup.number().required("Required"),
     occasion: Yup.string().required("Required"),
   });
 
   const formik = useFormik({
     initialValues: {
-      date: "",
-      time: "",
-      guests: 0,
-      occasion: "Anniversary"
+      date: '',
+      time: '',
+      guests: '',
+      occasion: ''
     },
     validationSchema: bookingSchema,
     onSubmit: async (values, { setSubmitting }) => {
       setSubmitting(true);
       setTimeout(() => {
+        submitData(JSON.stringify(values, null, 2));
         alert(JSON.stringify(values, null, 2));
         setSubmitting(false);
       }, 1000);
@@ -46,14 +80,14 @@ const BookForm = (props) => {
   return (
     <form
       onSubmit={formik.handleSubmit}
-      style={{ display: "grid", maxWidth: "400px", gap: "20px" }}
+      style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
     >
       <label htmlFor="res-date">Choose date</label>
       <input className={(formik.touched.date && formik.errors.date) && "invalid"}
         type="date"
         id="res-date"
         name="date"
-        {...formik.getFieldProps("date")}
+        {...{...formik.getFieldProps("date"), onChange: handleDateChange}}
       />
       {formik.touched.date && formik.errors.date ? (
         <div className="error">{formik.errors.date}</div>
@@ -62,7 +96,8 @@ const BookForm = (props) => {
       <select className={(formik.touched.time && formik.errors.time) && "invalid"} 
         id="res-time"
         name="time"
-        {...formik.getFieldProps("time")}>
+        {...formik.getFieldProps("time")}
+        >
         {availableTimesOptions}
       </select>
       {formik.touched.time && formik.errors.time ? (
@@ -96,6 +131,7 @@ const BookForm = (props) => {
       <input type="submit" value="Make Your reservation"  disabled={formik.isSubmitting} />
 
     </form>
+
   );
 };
 
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,15 +1,61 @@
 import BookingForm from "./BookingForm";
+import { useState, useEffect, useReducer } from "react";
+import { fetchAPI, submitAPI } from '../utils/fakeAPI';
 
-const BookingPage = () => {
-    const availableTimes = [17, 18, 19, 20, 21, 22];
-    return (
-        <article>
-           <section>
-            <h1>Booking page</h1>
-            <BookingForm availableTimes={availableTimes} />
-           </section>
-        </article>
-    )
-}
+const initializeTimes = [];
 
-export default BookingPage;
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "updateTimes":
+        console.log('R state:', state);
+        return [[], ...fetchAPI(new Date(action.date))];
+        break;
+
+      case "initializeTimes":
+        return [...state, ...action.date ];
+        break;
+
+      default:
+        return initializeTimes;
+    }
+  };
+
+  const submitData = formData => {
+    const response = submitAPI(formData);
+    if (response) {
+      console.log('Submit DD::', response);
+      //alert(response);
+      //alert(JSON.stringify(response, null, 2));
+      //navigate(pages.get('confirmedBooking').path);
+    }
+  };
+
+const BookPage = () => {
+
+  const [availableTimes, dispatch] = useReducer(reducer, initializeTimes);
+
+  useEffect(() => {
+    const response = fetchAPI(new Date());
+    if(response.length !== 0) {
+      console.log(response);
+      dispatch({ type: "initializeTimes", date: response });
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   dispatch({ type: "initializeTimes" });
+  // }, []);
+
+  return (
+    <>
+      <h1>Booking</h1>
+      <BookingForm
+        availableTimes={availableTimes}
+        dispatchOnDateChange={dispatch}
+        submitData={submitData}
+      />
+    </>
+  );
+};
+
+export default BookPage;
